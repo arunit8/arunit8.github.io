fe8b4c0b3cef127e1717677723c9e0fedfa7416d
arunit8 <88131655+arunit8@users.noreply.github.com>
2025-01-10 03:20:56 +0000

    v2

---
+++ script.js
@@ -0,0 +1,6 @@
+// Form Submission Handler
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+    alert("Thank you for reaching out! We will get back to you soon.");
+      this.reset();
+      });
--- scripts.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-      event.preventDefault();
-        alert('Your message has been sent!');
-        });
-})
